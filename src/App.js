ed177a784b2a6ce3a58de0909419236c982926fe
Chase Miller <chasemillers@outlook.com>
2019-04-06 15:13:03 -0700

    added more code for text tool but text tool still not working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Split from './composition/Split';
+import './App.css'; 
+import Tooltip from './composition/Tooltip';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// make 2 tooltips here and another inside the App directly
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
+
+
+function App() {
+  return (
+    <main className='App'>
+      <Split className='left' flexBasis='2'>
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+      </Split>
+      <Split className='right'>
+        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  )
 }
 
 export default App;
